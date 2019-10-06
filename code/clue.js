db60af856d0cf10e7af872ddebeb8457f3e73c4a
Jennie Dalgren <jennie.dalgren@gmail.com>
2019-10-06 12:53:03 +0200

    add starter code and assets

---
+++ code/clue.js
@@ -0,0 +1,64 @@
+// CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
+
+const mrGreen = {
+  firstName: "Jacob",
+  lastName: "Green",
+  color: "green",
+  description: "He has a lot of connections",
+  age: 45,
+  image: "assets/green.png",
+  occupation: "Entrepreneur"
+}
+
+// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS THE THE WEAPONS IF YOU LIKE.
+
+const rope = {
+  name: "rope",
+  weight: 10
+}
+
+// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
+
+// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS.
+
+const suspects = [
+  mrGreen,
+  mrsWhite,
+  profPlum,
+  missScarlet,
+  mrsPeacock,
+  mrMustard
+]
+
+const weapons = []
+
+const rooms = []
+
+// THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
+const randomSelector = array => {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+// CREATE AN OBJECT THAT KEEPS THE MYSTERY.
+// With a killer, a weapon and a room.
+// The values will be set later.
+
+// FINISH THIS FUNCTION TO SHOW ALL INFORMATION ABOUT THE KILLER.
+// This function will be invoked when you click on the killer card.
+const pickKiller = () => {
+  // This will randomly select a killer from the suspects. And add that to the mystery object.
+  mystery.killer = randomSelector(suspects)
+
+  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer.
+  const theKiller = document.getElementById("killer")
+  const theKillerName = document.getElementById("killerName")
+
+  theKiller.style.background = mystery.killer.color
+  theKillerName.innerHTML =
+    mystery.killer.firstName + " " + mystery.killer.lastName
+}
+
+// CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
+
+// CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
+// 'The murder was committed by Jacob Green, in the living room with a rope.'
